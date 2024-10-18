58f969b134a4f34c3e8c61afb0b1ead85193b003
sl-Obi-the-coder <rashid.jr.arrabi@icloud.com>
2024-10-17 22:29:49 -0700

    Add code to print the first 10 prime numbers and create a list of primes multiplied by 2

---
+++ pro 1.py
@@ -0,0 +1,29 @@
+print("hello baba")
+
+# print the first 10 prime number
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+primes = []
+num = 2
+
+while len(primes) < 10:
+    if is_prime(num):
+        primes.append(num)
+    num += 1
+
+#print(primes)
+
+for i in [1,2,3]:
+    print(i)
+
+#create list of primes multipled by 2
+primes_multiplied_by_2 = [p * 2 for p in primes]
+print(primes_multiplied_by_2)
+
